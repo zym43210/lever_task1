34f85bce8a2137dd24012817b086fd4e8128609e
zym43210 <zymzom@yandex.ru>
2020-02-07 23:13:36 +0300

    rewrite code in oop style

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
+import sys
 import json
+from xml.dom import minidom
+
+class file_worker:
+	@staticmethod
+	def read_from_file(path):
+		read_file=	open(path,"r")
+		data = json.load(read_file)
+		return data
+	@staticmethod
+	def write_to_file_json(object_for_dumping):
+		with open("data_file.json", "w") as write_file:
+			json.dump(object_for_dumping,write_file, sort_keys=True,indent=4)
+	@staticmethod
+	def write_to_file_xml(object_for_dumping):
+		with open("minidom_example.xml", "w") as f:
+	   		f.write(str(object_for_dumping))
 
 
-read_file_rooms=open("rooms.json","r")
-read_file_students=open("students.json","r")
-data_rooms = json.load(read_file_rooms)
-data_students = json.load(read_file_students)
-studentslist_in_room=[]
-list_with_students=[]
+class union_of_files:
+	@staticmethod
+	def creating_list_of_dicts(data_rooms,data_students):
+		list_with_students=[]
+		for room_number in data_rooms:
+			res=room_number["name"].split('#')
+			dict_with_students={"roomNumber":res[-1]}
+			list_with_students.append(dict_with_students)
+			for students_name in data_students:
 
-for room_number in data_rooms:
-	res=room_number["name"].split('#')
-	dict_with_students={"roomNumber":res[-1]}
-	list_with_students.append(dict_with_students)
-	for students_name in data_students:
+				if students_name["room"]==int(res[-1]):
+					dict_with_students.update({students_name["name"]:"name"})
+		return list_with_students
+			
+		
 
-		if students_name["room"]==int(res[-1]):
-			dict_with_students.update({students_name["name"]:"name"})
 
-with open("data_file.json", "w") as write_file:
-    json.dump(list_with_students,write_file, sort_keys=True,indent=4)
+
+data_rooms = file_worker.read_from_file(sys.argv[1])
+data_students = file_worker.read_from_file(sys.argv[2])
+
+
+
+
+if int(sys.argv[3])==1:
+	file_worker.write_to_file_json(union_of_files.creating_list_of_dicts(data_rooms,data_students))
+else:
+	file_worker.write_to_file_xml(union_of_files.creating_list_of_dicts(data_rooms,data_students))
